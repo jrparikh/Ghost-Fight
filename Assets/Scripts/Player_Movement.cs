a28af964439a9d8259be9fb3f3c27307117e0c09
Jay Parikh <jrparikh@ucsc.edu>
2018-06-14 16:19:26 -0700

    Core mechanic started

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -6,13 +6,15 @@ public class Player_Movement : MonoBehaviour {
 
     public float speed;             //Floating point variable to store the player's movement speed.
 
-    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    public Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    public SpriteRenderer sprite;
 
     // Use this for initialization
     void Start()
     {
         //Get and store a reference to the Rigidbody2D component so that we can access it.
         rb2d = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
@@ -23,5 +25,18 @@ public class Player_Movement : MonoBehaviour {
 
         Vector3 movement = new Vector2(moveHorizontal, moveVertical);
         rb2d.velocity = movement * speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("Trigger");
+        if (col.gameObject.tag == "Bookshelf")
+        {
+            //if (Input.GetKeyDown("space"))
+            {
+                Debug.Log("Trigger");
+                sprite.enabled = false;
+            }
+        }
     }
 }
