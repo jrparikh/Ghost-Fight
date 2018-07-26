52995b718f9afac78d2bf0cde396516d82d602ae
brandonwuzherezz <bsamudio@ucsc.edu>
2018-07-25 18:39:30 -0700

    MoreShit

--- Assets/Scripts/PlayerSelection.cs
+++ Assets/Scripts/PlayerSelection.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerSelection : MonoBehaviour {
-
+	public static string Player1;
+	public static string Player2;
 	// Use this for initialization
 	void Start () {
-		
+		Player1 = null;
+		Player2 = null;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Player1 != null && Player2 != null) {
+			//if button pressed down go to game screen
+		} 
+
 	}
 }
